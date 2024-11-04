b5e3e39068681d962d0159ca4e141b46f2898d97
Abdullah Zia <amz1cflp99@gmail.com>
2024-11-03 20:51:55 -0500

    Added WASD movement controls

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
 // input code
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;  
   stdin.setRawMode(true);       
   stdin.setEncoding("utf8");    
@@ -10,8 +14,19 @@ const setupInput = function () {
 
 const handleUserInput = function (key) {
 
-  if (key === '\u0003') {  
-    process.exit();        
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+
+  if (key === 'w') {
+    console.log("Move: up");
+  } else if (key === 'a') {
+    console.log("Move: left");
+  } else if (key === 's') {
+    console.log("Move: down");
+  } else if (key === 'd') {
+    console.log("Move: right");
   }
 };
 
