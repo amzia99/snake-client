62a73361b8eaab0cb88e8979ac89ee78dd5b82f0
Abdullah Zia <amz1cflp99@gmail.com>
2024-11-03 19:27:51 -0500

    implemented the play.js function and got the snake on server

---
+++ play.js
@@ -0,0 +1,16 @@
+// play code
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "172.25.186.223", 
+    port: 50541, 
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
